72f6df30fc624afc57fba35c8805e925c4355b58
Greg Arroyo <garroyo@arroyocode.com>
2015-11-30 20:47:00 -0800

    app & bootstrap typescript

---
+++ src/typescript/app.ts
@@ -0,0 +1,11 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'app'
+})
+@View({
+    template: `<span>Hearthstone Explorer</span>`
+})
+
+class AppComponent { }
+bootstrap(AppComponent);
