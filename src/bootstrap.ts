1d676c449d428aeac749edf103f5aa7144da8bf8
arroyocode <garroyo@arroyocode.com>
2015-12-17 16:55:25 -0800

    enableProdMode addition pre-bootstrapping

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,7 @@
-import {provide} from 'angular2/core';
+import {provide, enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {appProviders} from './app.providers';
 import {AppComponent} from './app.component';
 
+enableProdMode();
 bootstrap(AppComponent, appProviders());
